a7ddf8371609a4fa34000a49b808a0e20d6e2afd
Carter <cbawsome77@gmail.com>
2017-04-03 21:20:26 -0500

    Add support for multiple hosts

--- misc/geolocate.rb
+++ misc/geolocate.rb
@@ -20,11 +20,16 @@ class MetasploitModule < Msf::Auxiliary
         of a given IP address. Even though an external API is being
         used, a API key is not needed for the module to work properly.
       },
-      'Author'         => [ 'Carter Brainerd <@thecarterb>' ],
+      'Author'         => [ 'Carter Brainerd <0xCB@protonmail.com>' ],
       'License'        => MSF_LICENSE
     ))
 
-    deregister_options( 'SSL', 'RPORT', 'VHOST', 'Proxies' )
+    register_options(
+      [
+        OptString.new("RHOSTS", [true, "A comma seperated list of addresses to scan", nil])
+      ])
+
+    deregister_options( 'RHOST', 'SSL', 'RPORT', 'VHOST', 'Proxies' )
   end
 
   def gmap_url(lat, long)
@@ -32,36 +37,41 @@ class MetasploitModule < Msf::Auxiliary
   end
 
   def run
+    raw_rhosts = datastore['RHOSTS']
+    rhosts = raw_rhosts.split(',')
+    rhosts.each do |host|
 
-    # Because we're requesting an external site, we aren't actually requesting `rhost`
-    # We're requesting the API and passing `rhost` in the URI.
-    # This is why Rex::Proto::Http is being used and not send_request_cgi from HttpClient
-    uri = normalize_uri('json', "#{rhost}")
-    cli = Client.new('freegeoip.net', 80, {}, false)  # The API uses only HTTP unfortunately
-    cli.connect
-    req = cli.request_cgi({ 'uri' => uri })
-    res = cli.send_recv(req)
+      host.strip!  # Just in cast there are spaces in there
 
-    if res.nil?
-      print_error('Got an empty response from the API')
-      return
+      # Because we're requesting an external site, we aren't actually requesting `rhost`
+      # We're requesting the API and passing `rhost` in the URI.
+      # This is why Rex::Proto::Http is being used and not send_request_cgi from HttpClient
+      uri = normalize_uri('json', "#{host}")
+      cli = Client.new('freegeoip.net', 80, {}, false)  # The API uses only HTTP unfortunately
+      cli.connect
+      req = cli.request_cgi({ 'uri' => uri })
+      res = cli.send_recv(req)
+
+      if res.nil?
+        print_error('Got an empty response from the API')
+        return
+      end
+
+      if res.code != 200
+        print_error("Got an unexpected response from the API (Code: #{res.code})")
+        return
+      end
+
+      parsed = JSON.parse(res.body)  # Make it fun to use (hash)
+
+      print_line
+      print_status("Data for #{host}")
+      print_line("  Country: #{parsed['country_name']} (#{parsed['country_code']})")  # Print the country and country country code
+      print_line("  City: #{parsed['region_name']} (#{parsed['region_code']})")       # Print the city and city code
+      print_line("  Zip Code: #{parsed['zip_code']}")                                 # Print the zip code
+      print_line("  Latitude: #{parsed['latitude']} (estimation)")                    # Print the latitude
+      print_line("  Longitude: #{parsed['longitude']} (estimation)")                  # Print the longitude
+      print_line("  Google Maps URL: #{gmap_url(parsed['latitude'], parsed['longitude'])}")
     end
-
-    if res.code != 200
-      print_error("Got an unexpected response from the API (Code: #{res.code})")
-      return
-    end
-
-    parsed = JSON.parse(res.body)  # Make it fun to use (hash)
-
-    print_status("Data for #{rhost}\n")
-    print_line("  Country: #{parsed['country_name']} (#{parsed['country_code']})")  # Print the country and country country code
-    print_line("  City: #{parsed['region_name']} (#{parsed['region_code']})")       # Print the city and city code
-    print_line("  Zip Code: #{parsed['zip_code']}")                                 # Print the zip code
-    print_line("  Latitude: #{parsed['latitude']} (estimation)")                    # Print the latitude
-    print_line("  Longitude: #{parsed['longitude']} (estimation)")                  # Print the longitude
-    print_line
-    print_good("Google Maps URL: #{gmap_url(parsed['latitude'], parsed['longitude'])}")
-    print_line
   end
 end
